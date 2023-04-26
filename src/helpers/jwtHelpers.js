72f3a949a4e1ff4a0c1c0115617869b1c41f82a4
PK <pradeepdhital124@gmail.com>
2023-04-26 13:42:49 +1000

    categories router initialised

--- src/helpers/jwtHelpers.js
+++ src/helpers/jwtHelpers.js
@@ -1,30 +1,30 @@
 import jwt from "jsonwebtoken";
-import { insertSession } from "../../sessions/SessionModel.js";
+import { insertSession } from "../models/sessions/SessionModel.js";
 import { updateOneAdminUser } from "../models/adminUserModel/AdminUserModel.js";
 
- const signAccessJWT = async (payload) => {
+const signAccessJWT = async (payload) => {
   const accessJWT = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
     expiresIn: "15m",
   });
   const obj = {
     token: accessJWT,
-    type: "jwt",
+    type: "accessJWT",
   };
   await insertSession(obj);
   return accessJWT;
 };
 
- const signRefreshJWT = async (payload) => {
+const signRefreshJWT = async (payload) => {
   const refreshJWT = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
     expiresIn: "30d",
   });
 
-  await updateOneAdminUser(payload, {refreshJWT});
+  await updateOneAdminUser(payload, { refreshJWT });
   return refreshJWT;
 };
 export const createJWTs = async (payload) => {
-    return {
-        accessJWT: await signAccessJWT(payload),
-        refreshJWT: await signRefreshJWT(payload)
-   } 
-}
+  return {
+    accessJWT: await signAccessJWT(payload),
+    refreshJWT: await signRefreshJWT(payload),
+  };
+};
---
+++ src/routers/categoriesRouter.js
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+router.get("/", (req, res, next) => {
+  try {
+  } catch (error) {}
+});
+
+export default router;
