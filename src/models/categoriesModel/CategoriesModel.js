611859cc0e9ca2c7b9c005cb3060550de11fd8b5
PK <pradeepdhital124@gmail.com>
2023-05-02 22:49:38 +1000

    working on categories router'

--- src/models/categoriesModel/CategoriesModel.js
+++ src/models/categoriesModel/CategoriesModel.js
@@ -1,4 +1,13 @@
 import categoriesSchema from "./CategoriesSchema.js";
+// post categories
 export const insertCategory = (obj) => {
   return categoriesSchema(obj).save();
+};
+// get one category
+export const getCategoryById = (_id) => {
+  return categoriesSchema.findById(_id);
+};
+// get all categories
+export const getAllCategories = () => {
+  return categoriesSchema.find();
 };
--- src/routers/categoriesRouter.js
+++ src/routers/categoriesRouter.js
@@ -1,15 +1,35 @@
 import express from "express";
 import slugify from "slugify";
-import { insertCategory } from "../models/categoriesModel/CategoriesModel.js";
+import {
+  getAllCategories,
+  getCategoryById,
+  insertCategory,
+} from "../models/categoriesModel/CategoriesModel.js";
 import { newCategoryValidation } from "../middlewares/joivalidation/joiValidation.js";
 const router = express.Router();
+// get categories
+router.get("/:_id?", async (req, res, next) => {
+  try {
+    const { _id } = req.params;
+    const categories = _id
+      ? await getCategoryById(_id)
+      : await getAllCategories();
+    res.json({
+      status: "success",
+      message: "category list",
+      categories,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 // post new category
 router.post("/", newCategoryValidation, async (req, res, next) => {
   try {
     req.body.slug = slugify(req.body.name, {
       lower: true,
-      trim:true,
-    })
+      trim: true,
+    });
     const result = await insertCategory(req.body);
 
     console.log(req.body);
@@ -26,5 +46,6 @@ router.post("/", newCategoryValidation, async (req, res, next) => {
     next(error);
   }
 });
+
 
 export default router;
